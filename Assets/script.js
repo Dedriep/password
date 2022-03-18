b26879cdd9f504e41ef81b7855e7deed23e44e68
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-03-17 21:04:00 -0400

    restructured code

--- Assets/script.js
+++ Assets/script.js
@@ -1,40 +1,63 @@
 // Assignment code here
-var value = Math.floor(Math.random()* 20 + 6);
-
-console.log(value + 10)
-
-
-function generatePassword (password){
-  var symbols = window.prompt("Do you want symbols?") 
-    if (symbols === "yes" || symbols === "YES") { 
-        if (value === 2) { var symbols = "&"}
-        else if (value === 4) { var symbols = "!"}
-        else if (value === 6) { var symbols = "@"}
-        else if (value === 8) { var symbols = "#"}
-        else if  (value === 10) { var symbols = "$"}
-        else if (value === 12) { var symbols = "%"}
-        else if (value === 14) { var symbols = "^"}
-        else if (value === 16) { var symbols = "*"}
-        else if (value === 18) { var symbols = "("}
-        else if ( value === 20) { var symbol = ")"}
-        else {var symbols = "?"}
+var symbolOptions = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var capitalOptions =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numberOptions = [1,2,3,4,5,6,7,8,9,0]
+//var lowerCaseOptions = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+//var lowerCase = toLowerCase(capitalOptions)
+console.log(numberOptions)
+var password = symbolOptions + capitalOptions + lowerCaseOptions + numberOptions
 
 
-    } else if (symbols === "no" || symbols === "NO") {
-        var symbols = "" || null;
+
+function generatePassword(password) {
+  var symbols = window.prompt("Do you want symbols?")
+  if (symbols === "yes"|| "YES") {
+    return symbolOptions
+  } else if (symbols === "no" || symbols === "NO") {
+    return symbolOptions = ""
+} else {window.prompt("Enter YES or NO") };
+
+
+  var capital = window.prompt("Do you want UPPERCASE letters?")
+  if (capital === "yes" || "YES") {
+    return capitalOptions
+  } else if (symbols === "no" || symbols === "NO") {
+    return capitalOptions = ""
+  } else { window.prompt("Enter YES or NO") };
+
+
+  var lowerCase = window.prompt("Do you want symbols?")
+  if (lowerCase === "yes"|| "YES") {
+    return lowerCaseOptions
+  } else if (lowerCase === "no" || symbols === "NO") {
+    return lowerCaseOptions = ""
+} else {window.prompt("Enter YES or NO") };
+
+var numbers = window.prompt("Do you want symbols?")
+if (symbols === "yes"|| "YES") {
+  return numberOptions
+} else if (numbers === "no" || numbers === "NO") {
+  return numberOptions = ""
+} else {window.prompt("Enter YES or NO") };
+
+
+
+var passwordLength = window.prompt ("How many charcters do you want your password? Please enter a number between 10 and 30?") 
+  if (passwordLength >= 10 && passwordLength >= 30){
+  return passwordLength
+  } else {
+    window.prompt("Please enter a number between 10 and 30!")
+  }
+
+for ( var i = 0 ; i< passwordLength; i++) {
+
   
-        return password;
-    } else {window.prompt("Enter YES or NO")};
-  
 
-    var capitalLetters = window.prompt ("Do you want UPPERCASE letters?") 
-      
 
-    var smallLetters = window.prompt ("Do you want lowercase letters?")
+  return password;
 
-    var numbers = window.prompt ("Do you want numbers?")
+}
 
-    password = symbol + capitalLetter + smallLetters + numbers
 
 }
 
@@ -55,6 +78,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+generateBtn.addEventListener("click", writePassword);
 
 
